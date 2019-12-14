01c3cc9b035a0c7dbec1ddcb25971dfc6dce0c00
Christopher <cyackley202@gmail.com>
2019-12-14 12:23:08 -0500

    WackyVariableNonAssignment is the file containing the final version of the project.

---
+++ GrandCircusDeliverableOnePossibleFinal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Delivarable_One_for_Grand_Circus_Draft_Two
+{
+    class Deliverable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you prefer a tropical, musical, or adventurous vacation?");
+            string vacationType = Console.ReadLine();
+            string travelType;
+            string vacationDestination;
+            if (vacationType == "tropical")
+            {
+                vacationDestination = "Mexico";
+            }
+            else if (vacationType == "musical")
+            {
+                vacationDestination = "New Orleans";
+            }
+            else
+            {
+                vacationDestination = "The Grand Canyon";
+            }
+            
+
+            Console.WriteLine("Please enter, in numerical form, the number of people (including you!) who will be traveling.");
+            int groupSize = int.Parse(Console.ReadLine());
+            if (groupSize >= 6)
+            {
+                travelType = "Charter Flight";
+            }
+            else if (groupSize >= 3)
+            {
+                travelType = "Helicopter";
+            }
+            else
+            {
+                travelType = "First Class";
+            }
+
+            string result = ("Because you prefer a " + vacationType + " vacation, and you have " + groupSize + " person/people going on the trip, we recommend a " + travelType + " to " + vacationDestination + ".");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ WackyVariableNonAssignment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Delivarable_One_for_Grand_Circus_Draft_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Would you prefer a tropical, musical, or adventurous vacation?");
+            string travelType;
+            string vacationDestination;
+            string vacationType = Console.ReadLine(); 
+            if (vacationType == "tropical")
+            {
+                vacationDestination = "Mexico";
+            }
+            else if (vacationType == "musical")
+            {
+                vacationDestination = "New Orleans";
+            }
+            else
+            {
+                vacationDestination = "The Grand Canyon";
+            }
+        
+
+            Console.WriteLine("Please enter, in numerical form, the number of people (including you!) who will be traveling.");
+            int groupSize = int.Parse(Console.ReadLine());
+            if (groupSize >= 6)
+            {
+                travelType = "Charter Flight";
+            }
+            else if (groupSize >= 3)
+            {
+                travelType = "Helicopter";
+            }
+            else
+            {
+                travelType = "a First Class flight";
+            }
+
+            string result = "Because you prefer a " + vacationType + " vacation, and you have " + groupSize + " person/people going on the trip, we recommend a " + travelType + " to " + vacationDestination + ".";
+
+            Console.WriteLine(result);
+        }
+    }
+}
